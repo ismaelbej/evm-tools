76f0d32a57f48d35c1f2a3382c0374f1b876718a
Ismael Bejarano <ismaelbej@gmail.com>
2022-10-03 01:50:21 -0300

    Add get_it as dependency

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'presentation/app.dart';
+import 'services.dart';
 
-void main() {
+void main() async {
+  await initializeServices();
   runApp(const ProviderScope(child: MyApp()));
 }
